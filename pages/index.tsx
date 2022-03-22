b046e61af115c1de1d0cbc74a02844b4f5dde6b9
progblog <furuie@goodbee.jp>
2022-03-22 23:44:00 +0900

    domが取得できない

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,25 @@
 import type { NextPage } from 'next'
 import axios from 'axios'
-
-axios.get(`${process.env.NEXT_PUBLIC_BASE_URL}`, {
-  headers: { 'x-microcms-api-key': `${process.env.NEXT_PUBLIC_API_KEY}` },
-}).then(response => {
-  console.log(response)
-})
-
+import { useEffect, useState } from 'react'
 
 const Home: NextPage = () => {
+  const [articles, serArticles] = useState([]);
+  useEffect(() => {
+    axios.get(`${process.env.NEXT_PUBLIC_BASE_URL}`, {
+      headers: { 'x-microcms-api-key': `${process.env.NEXT_PUBLIC_API_KEY}` },
+    }).then(response => {
+      serArticles(response.data.contents)
+    })
+  }, [])
   return (
     <>
       <h1>記事一覧</h1>
+      {articles}
+      {/* <ul>
+        {articles.map((article: any) => {
+          <li key={article.id}>{article.title}</li>
+        })}
+      </ul> */}
     </>
   )
 }
