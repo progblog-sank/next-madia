80248b2c9f4b6030007dec96bbe4551af3022ae7
progblog <furuie@goodbee.jp>
2022-03-23 09:19:45 +0900

    表示まで

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,27 +1,27 @@
-import type { NextPage } from 'next'
-import axios from 'axios'
-import { useEffect, useState } from 'react'
+import type { NextPage } from "next";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 const Home: NextPage = () => {
   const [articles, serArticles] = useState([]);
   useEffect(() => {
-    axios.get(`${process.env.NEXT_PUBLIC_BASE_URL}`, {
-      headers: { 'x-microcms-api-key': `${process.env.NEXT_PUBLIC_API_KEY}` },
-    }).then(response => {
-      serArticles(response.data.contents)
-    })
-  }, [])
+    axios
+      .get(`${process.env.NEXT_PUBLIC_BASE_URL}`, {
+        headers: { "x-microcms-api-key": `${process.env.NEXT_PUBLIC_API_KEY}` },
+      })
+      .then((response) => {
+        serArticles(response.data.contents);
+      });
+  }, []);
   return (
     <>
-      <h1>記事一覧</h1>
-      {articles}
-      {/* <ul>
-        {articles.map((article: any) => {
+      <ul>
+        {articles.map((article: any) => (
           <li key={article.id}>{article.title}</li>
-        })}
-      </ul> */}
+        ))}
+      </ul>
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
