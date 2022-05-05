16bebe418321addf8a7a54745252443f1f607411
progblog <furuie@goodbee.jp>
2022-05-05 11:09:12 +0900

    詳細ページ表示まで

--- src/pages/media/[slug].tsx
+++ src/pages/media/[slug].tsx
@@ -1,10 +1,27 @@
 import type { NextPage } from "next";
 
 
-const MediaSlug: NextPage = () => {
-    return <p>詳細ページ</p>
+// @ts-ignore
+const MediaSlug: NextPage = ({ article }) => {
+    const createMarkup = () => {
+        return { __html: article.body };
+    };
+    return (
+        <>
+            <h2>{article?.title}</h2>
+            <div className="content" dangerouslySetInnerHTML={createMarkup()} />
+        </>
+    )
 };
 
+
+const url = process.env.NEXT_PUBLIC_BASE_URL as string
+const key = { headers: { "x-microcms-api-key": `${process.env.NEXT_PUBLIC_API_KEY}` } }
+export async function getServerSideProps() {
+    const json = await fetch(url, key).then((r) => r.json());
+    const article = json.contents[0];
+    return { props: { article } };
+}
 
 
 export default MediaSlug;
