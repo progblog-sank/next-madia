06b077bbfee05672d177e20c0618480d7f2a46eb
progblog <furuie@goodbee.jp>
2022-05-05 22:32:38 +0900

    emvを辞める

--- src/pages/media/[slug].tsx
+++ src/pages/media/[slug].tsx
@@ -14,8 +14,8 @@ const MediaSlug: NextPage = ({ article }) => {
     )
 };
 
-const url = process.env.BASE_URL as string
-const key = { headers: { "x-microcms-api-key": `${process.env.API_KEY}` } }
+const url = 'https://create-next.microcms.io/api/v1/media'
+const key = { headers: { "x-microcms-api-key": '797ad38d37554e9297d8feb731866802a550' } }
 // @ts-ignore
 export async function getServerSideProps(context) {
     const json = await fetch(url + '/' + context.query.slug, key).then((r) => r.json());
