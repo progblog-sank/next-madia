f66c9ae987e1fd6dd843d4de2f4b3e1b284837a9
progblog <furuie@goodbee.jp>
2022-05-05 10:57:34 +0900

    詳細ページ作成まで

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,33 +1,35 @@
 import type { NextPage } from "next";
+import Link from "next/link";
+interface Article {
+  id: string,
+  title: string
+}
 
-const Home: NextPage = (articles: any) => {
-
+// @ts-ignore
+const Home: NextPage = ({ articles }) => {
   if (articles.length > 0) {
     return (
-      <>
-        <ul>
-          {articles.map((article: any) => (
-            <li key={article.id}>{article.title}</li>
-            ))}
-          </ul>
-      </>
-    );
+      <ul>
+        {articles.map((article: Article) => {
+          return <li key={article.id}> <Link href={'/media/' + article.id}><a>{article.title}</a></Link></li>;
+        })}
+      </ul>
+    )
   } else {
-    return <div>loading......</div>
+    return <p>loading.....</p>
   }
 
+
 };
+
 
 const url = process.env.NEXT_PUBLIC_BASE_URL as string
 const key = { headers: { "x-microcms-api-key": `${process.env.NEXT_PUBLIC_API_KEY}` } }
 export async function getServerSideProps() {
   const json = await fetch(url, key).then((r) => r.json());
   const articles = json.contents;
-  return {
-    props: {
-      articles,
-    },
-  };
+  return { props: { articles } };
 }
+
 
 export default Home;
---
+++ src/pages/media/[slug].tsx
@@ -0,0 +1,10 @@
+import type { NextPage } from "next";
+
+
+const MediaSlug: NextPage = () => {
+    return <p>詳細ページ</p>
+};
+
+
+
+export default MediaSlug;
