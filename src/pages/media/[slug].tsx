7c520ac4a0872485e4c5d821df11c624f62ed8a8
progblog <furuie@goodbee.jp>
2022-05-05 22:21:18 +0900

    env をサーバー側で呼び出す

--- src/pages/media/[slug].tsx
+++ src/pages/media/[slug].tsx
@@ -2,18 +2,26 @@ import type { NextPage } from "next";
 
 
 // @ts-ignore
-const MediaSlug: NextPage = () => {
-    return <p>hoge</p>
+const MediaSlug: NextPage = ({ article }) => {
+    const createMarkup = () => {
+        return { __html: article.body };
+    };
+    return (
+        <>
+            <h2>{article?.title}</h2>
+            <div className="content" dangerouslySetInnerHTML={createMarkup()} />
+        </>
+    )
 };
 
-// const url = process.env.NEXT_PUBLIC_BASE_URL as string
-// const key = { headers: { "x-microcms-api-key": `${process.env.NEXT_PUBLIC_API_KEY}` } }
-// // @ts-ignore
-// export async function getServerSideProps(context) {
-//     const json = await fetch(url + '/' + context.query.slug, key).then((r) => r.json());
-//     const article = json;
-//     return { props: { article } };
-// }
+const url = process.env.BASE_URL as string
+const key = { headers: { "x-microcms-api-key": `${process.env.API_KEY}` } }
+// @ts-ignore
+export async function getServerSideProps(context) {
+    const json = await fetch(url + '/' + context.query.slug, key).then((r) => r.json());
+    const article = json;
+    return { props: { article } };
+}
 
 
 export default MediaSlug;
