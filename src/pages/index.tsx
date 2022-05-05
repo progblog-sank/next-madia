1a93daae7f9daae58a54685d284fe81912dc900c
progblog <furuie@goodbee.jp>
2022-05-05 22:13:51 +0900

    いったんテスト

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,30 +6,20 @@ interface Article {
 }
 
 // @ts-ignore
-const Home: NextPage = ({ articles }) => {
-  if (articles.length > 0) {
-    return (
-      <ul>
-        {articles.map((article: Article) => {
-          return <li key={article.id}> <Link href={'/media/' + article.id}><a>{article.title}</a></Link></li>;
-        })}
-      </ul>
-    )
-  } else {
-    return <p>loading.....</p>
-  }
+const Home: NextPage = () => {
+  return <p>hoge</p>
 
 
 };
 
 
-const url = process.env.NEXT_PUBLIC_BASE_URL as string
-const key = { headers: { "x-microcms-api-key": `${process.env.NEXT_PUBLIC_API_KEY}` } }
-export async function getServerSideProps() {
-  const json = await fetch(url, key).then((r) => r.json());
-  const articles = json.contents;
-  return { props: { articles } };
-}
+// const url = process.env.NEXT_PUBLIC_BASE_URL as string
+// const key = { headers: { "x-microcms-api-key": `${process.env.NEXT_PUBLIC_API_KEY}` } }
+// export async function getServerSideProps() {
+//   const json = await fetch(url, key).then((r) => r.json());
+//   const articles = json.contents;
+//   return { props: { articles } };
+// }
 
 
 export default Home;
