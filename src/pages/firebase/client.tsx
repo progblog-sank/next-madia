96b4d816d055e046b247844578d98f9e119d918b
progblog <furuie@goodbee.jp>
2022-05-14 16:09:16 +0900

    もっと見るを実装

--- src/pages/firebase/client.tsx
+++ src/pages/firebase/client.tsx
@@ -1,27 +1,42 @@
 import type { NextPage } from 'next'
 import { useEffect, useState } from 'react';
 import firebase from '../../utils/firebase/clientInit';
-import { doc, getDoc, getFirestore } from "firebase/firestore";
+import { collection, doc, getDoc, getDocs, getFirestore, limit, orderBy, query, startAfter, where } from "firebase/firestore";
 
 
 
 const CLIENTFIRE: NextPage = () => {
 
-  const [books, setBook] = useState<any>({})
+  const [books, setBook] = useState<any>([])
+  const [lastQuery, setLastQuery] = useState<any>()
+
+
 
   const fetchFireStore = async () => {
     const db = getFirestore(firebase);
-    console.log(db)
-    const docRef = doc(db, "basic_relay", "3JNpSoaDqHnnD26lysKY");
-    const docSnap = await getDoc(docRef);
+    const q = query(collection(db, "basic_relay"), orderBy("time_stamp", "asc"), limit(2));
+    const querySnapshot = await getDocs(q);
+    const documentSnapshots = await getDocs(q);
+    setLastQuery(documentSnapshots.docs[documentSnapshots.docs.length - 1])
+    const tempArray = new Array
+    querySnapshot.forEach((doc) => {
+      tempArray.push(doc.data())
+    });
+    setBook(tempArray)
+  }
 
-    if (docSnap.exists()) {
-      console.log("Document data:", docSnap.data());
-      setBook(docSnap.data())
-    } else {
-      // doc.data() will be undefined in this case
-      console.log("No such document!");
-    }
+  const addBooks = async () => {
+    const db = getFirestore(firebase);
+    const q = query(collection(db, "basic_relay"), orderBy("time_stamp", "asc"), startAfter(lastQuery), limit(1));
+
+    const querySnapshot = await getDocs(q);
+    const documentSnapshots = await getDocs(q);
+    setLastQuery(documentSnapshots.docs[documentSnapshots.docs.length - 1])
+    const tempArray = books
+    querySnapshot.forEach((doc) => {
+        tempArray.push(doc.data())
+    });
+    setBook(([...tempArray]))
   }
 
   useEffect(() => {
@@ -33,9 +48,17 @@ const CLIENTFIRE: NextPage = () => {
 
   return (
     <>
-      <p><img src={books.book_img} alt={books.book_title} /></p>
-      <p>{books.book_title}</p>
-      <p>{books.book_impression}</p>
+      {books.map((el: any) => {
+        return (
+          <div key={el.book_amazon}>
+            <p><img src={el.book_img} alt={el.book_title} /></p>
+            <p>{el.book_title}</p>
+            <p>{el.book_impression}</p>
+          </div>
+        )
+      })}
+
+      <button onClick={addBooks}>追加する</button>
     </>
   )
 
