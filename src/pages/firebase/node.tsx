cb38f0b7754804166f4ba67158967a42029a34e4
progblog <furuie@goodbee.jp>
2022-05-15 15:51:42 +0900

    複数のAPIをSSRで取得できることを確認

--- src/pages/firebase/node.tsx
+++ src/pages/firebase/node.tsx
@@ -5,17 +5,23 @@ import admin from '../../utils/firebase/nodeInit'
 export async function getServerSideProps() {
   const db = admin.firestore();
   const collection = db.collection('basic_relay');
+  const collection_2 = db.collection('second_relay');
   const collectionArray = await collection.doc('3JNpSoaDqHnnD26lysKY').get();
+  const collectionArray_2 = await collection_2.doc('0j1nl71Ppy8w83af2O00').get();
   const data = collectionArray.data()
+  const data_2 = collectionArray_2.data()
   const jsonData = JSON.parse(JSON.stringify(data));
-  return { props: { jsonData } };
+  const jsonData_2 = JSON.parse(JSON.stringify(data_2));
+  console.log(jsonData_2)
+  return { props: { jsonData, jsonData_2 } };
 }
 
 
 // @ts-ignore
-const NODEFIRE: NextPage = ({ jsonData }) => {
+const NODEFIRE: NextPage = ({ jsonData, jsonData_2 }) => {
   return (
     <>
+      <p>{ jsonData_2.name}</p>
       <p><img src={jsonData.book_img} alt={jsonData.book_title} /></p>
       <p>{jsonData.book_title}</p>
       <p>{jsonData.book_impression}</p>
